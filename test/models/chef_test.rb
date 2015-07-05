8d6651ff317eba974b11fb0bb37d92d4a1e3a898
o3dwade <omarfarooq93@yahoo.com>
2015-07-05 10:47:53 +0000

    create chefs and recipes association and tests

---
+++ test/models/chef_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+
+class ChefTest < ActiveSupport::TestCase
+   def setup
+        @chef= Chef.new(chefname: "john", email: "john@example.com")
+   end
+   
+   test "chef should be valid" do
+    assert @chef.valid?
+   end
+   
+   test "chefname should be present" do
+    @chef.chefname=""
+    assert_not @chef.valid?
+   end
+   
+   test "chefname should not be too long" do
+   end
+   
+   test "chefname should not be too short" do
+   end
+   
+end
---
+++ test/models/recipe_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class RecipeTest < ActiveSupport::TestCase
+   
+   def setup
+       @recipe = Recipe.new(name: "chicken parm", summary: "this is the best chicken parm recipe ever", description: "Heat oil, add onion, add tomato sauce
+       add chicken, cook for 20 minutes")
+   end
+   
+   test "Recipe should be valid" do
+       assert @recipe.valid?
+   end
+   
+   test "name should be present" do
+        @recipe.name = " "
+        assert_not @recipe.valid?
+   end
+   
+   test "name length should not be too long" do
+      
+   end
+   
+       
+       
+end
